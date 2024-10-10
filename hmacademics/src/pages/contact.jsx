87167c1f671c58a6fb186d280ba7e3fc6dbe3340
hayleypyle <hstowell@icloud.com>
2024-10-10 19:38:05 -0400

    fixed linking of homepage and contacts

--- hmacademics/src/pages/contact.jsx
+++ hmacademics/src/pages/contact.jsx
@@ -4,9 +4,13 @@ import './styles.css'
 
 export default function Contact(){
     return(
+        <>
         <header>
+            <Link to="/">Home</Link>
+            <Link to="/contact">Contact Us</Link>
             <p>Contact Us!</p>
         </header>
+        </>
 
     )
 }
--- hmacademics/src/pages/homepage.jsx
+++ hmacademics/src/pages/homepage.jsx
@@ -4,9 +4,14 @@ import './styles.css'
 
 export default function Home(){
     return(
+        <>
         <header>
-            <h1>Home Page</h1>
+            <Link to="/">Home</Link>
+            <Link to="/contact">Contact Us</Link>
+            <h1>H&M Academics</h1>
         </header>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore magnam quibusdam laborum rem excepturi aperiam mollitia, ea ad repudiandae corporis dolores expedita molestias velit porro voluptate enim magni consequatur asperiores tempora rerum impedit adipisci amet cumque. Culpa explicabo laudantium, est maxime reprehenderit incidunt maiores aliquid? Totam hic rem veritatis quasi!</div>
+        </>
 
     )
 }
