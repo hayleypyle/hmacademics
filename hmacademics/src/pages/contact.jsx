4694afe33264a42ce1a2dfa9f108bcc623bf71f2
hayleypyle <hstowell@icloud.com>
2024-10-10 19:33:11 -0400

    added react router dom, added pages

---
+++ hmacademics/src/pages/contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './styles.css'
+
+export default function Contact(){
+    return(
+        <header>
+            <p>Contact Us!</p>
+        </header>
+
+    )
+}
---
+++ hmacademics/src/pages/homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './styles.css'
+
+export default function Home(){
+    return(
+        <header>
+            <h1>Home Page</h1>
+        </header>
+
+    )
+}
