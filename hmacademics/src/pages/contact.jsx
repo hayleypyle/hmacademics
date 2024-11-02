0aa4d2a80a118c6f7dccf01d5a58e876c3a3f882
hayleypyle <hstowell@icloud.com>
2024-11-02 14:32:38 -0400

    styled contact page in css

--- hmacademics/src/pages/contact.jsx
+++ hmacademics/src/pages/contact.jsx
@@ -9,29 +9,38 @@ export default function Contact(){
             <Link to="/">Home</Link>
             <Link to="/contact">Contact Us</Link>
         </header>
-        <div className="contactForm"> <p>Contact us with the form below. We will 
+        <div className="contactForm">
+            <div className="contactForm-inner"> <p>Contact us with the form below. We will 
             reach out to you to confirm a day and time.</p>
         <form>
-            <input type="text" placeholder="Name" required/>
-            <input type="email" placeholder="Email" required/>
-            <input type="tel" placeholder="(999)999-9999" required/>
+            <div className="form-input"><input type="text" placeholder="Name" required/></div>
+            <div className="form-input"><input type="email" placeholder="Email" required/></div>
+            <div className='form-input'><input type="tel" placeholder="(999)999-9999" required/></div>
+            <div  className="contact-pref">
             <p>How do you prefer to be contacted? </p>
-            <input type="checkbox" for ="email"/>
-            <label for ="email">Email</label>
-            <p></p><input type="checkbox" for ="text"/>
-            <label for ="text">Text</label>
-            <p></p><input type="checkbox" for ="call"/>
-            <label for ="call">Phone Call</label>
+            <input type="checkbox" for ="email"/><label for ="email">Email</label>
+            <input type="checkbox" for ="text"/><label for ="text">Text</label>
+            <input type="checkbox" for ="call"/><label for ="call">Phone Call</label>
+            </div>
             
-            <p></p>
+        
             <label for ="class">What class and topics are you needing tutoring for? </label>
-            <p></p><textarea id="class" rows="5" cols="33" placeholder="Example: I'm taking Algebra 1 and struggling with systmes of equations" required/>
-            <p></p><label for ="details">Tell us what days and times work best for you. </label>
-            <p></p><textarea id="details" rows="5" cols="33"  required/>
+            <div className='form-input'><textarea id="class" rows="5" cols="33" placeholder="Example: I'm taking Algebra 1 and struggling with systmes of equations" required/>
+            </div>
+            <label for ="details">Tell us what days and times work best for you. </label>
+            <div className='form-input'><textarea id="details" rows="5" cols="33"  required/>
+            </div>
     
             <button type="submit">Contact Us</button>
         </form>
         </div>
+        </div>
+        <footer>
+            <p>H&M Academics</p>
+            <Link to="/contact">Contact Us</Link>
+        </footer>
+        
+
         </>
 
     )
